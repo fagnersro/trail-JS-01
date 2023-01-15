ad36fd326f560d85f97ec4f5d4359da63ad0d99c
fagnersro <fagner.hg40@gmail.com>
2023-01-15 00:29:32 -0300

    adição de atributos e métodos / construtor e encapsulamento, getters e setters, trabalhando com datas

--- Calculadora/curso-javascript-projeto-calculadora-clone/scripts/controller/calcController.js
+++ Calculadora/curso-javascript-projeto-calculadora-clone/scripts/controller/calcController.js
@@ -1,1 +1,29 @@
-class CalcController {}
+class CalcController {
+  constructor() {
+    this._displayCalcEl = document.querySelector('#display');
+    this._dateEl = document.querySelector('#data');
+    this._timeEl = document.querySelector('#hora');
+
+    this._currentDate;
+    this.initialize();
+  }
+
+  initialize() {
+    this._dateEl.innerHTML = '09/02/1995';
+    this._timeEl.innerHTML = '23:42';
+  }
+
+  get displayCalc() {
+    return this._displayCalcEl.innerHTML;
+  }
+  set displayCalc(valor) {
+    this._displayCalcEl.innerHTML = valor;
+  }
+
+  get currentDate() {
+    return new Date();
+  }
+  set currentDate(data) {
+    this._currentDate = data;
+  }
+}
