236074a5295962d4195fc0b8ff7329fe77c00c21
fagnersro <fagner.hg40@gmail.com>
2023-01-16 23:58:38 -0300

    inicializando no projeto de usuarios revendo comandos de seletores

---
+++ curso-javascript-projeto-usuarios/index.js
@@ -0,0 +1,10 @@
+const nome = document.querySelector('#exampleInputName');
+const gender = document.querySelectorAll(
+  '#form-user-create [name=gender]:checked'
+);
+const birth = document.querySelector('#exampleInputBirth');
+const country = document.querySelector('#exampleInputCountry');
+const email = document.querySelector('#exampleInputEmail');
+const password = document.querySelector('#exampleInputPassword');
+const photo = document.querySelector('#exampleInputFile');
+const admin = document.querySelector('#exampleInputAdmin');
