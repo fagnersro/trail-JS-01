7cfb1ec33827b04d9f7ed4a88518ff031e8c4592
fagnersro <fagner.hg40@gmail.com>
2023-01-22 18:12:34 -0300

    Iniciando NodeJS da trilha configurando ambiente trabalhando com rotas

---
+++ NodeJs/restfull/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const consign = require('consign');
+
+let app = express();
+
+consign().include('routes').into(app);
+
+app.listen(3000, 'localhost', () => {
+  console.log('Servidor rodando!');
+});
