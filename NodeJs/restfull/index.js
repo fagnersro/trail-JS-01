60c9818fc9c8476e5f9ea484bdc75feb7a494378
fagnersro <fagner.hg40@gmail.com>
2023-01-24 00:37:48 -0300

    Concluindo seção 4 / nodeJs / restfull API

--- NodeJs/restfull/index.js
+++ NodeJs/restfull/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const consign = require('consign');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
 
 let app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(expressValidator());
 
-consign().include('routes').into(app);
+consign().include('routes').include('utils').into(app);
 
 app.listen(3000, 'localhost', () => {
   console.log('Servidor rodando!');
