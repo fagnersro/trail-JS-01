37b86c84acb982f0cd0f6fe4468c904f8cdf5084
fagnersro <fagner.hg40@gmail.com>
2023-01-25 09:12:40 -0300

    Projeto Usuário com servidor finalizado

--- project-node-clint-server/back-end-usuario-rest-api/restfull/index.js
+++ project-node-clint-server/back-end-usuario-rest-api/restfull/index.js
@@ -4,12 +4,12 @@ const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
 
 let app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false, limit: '50mb' }));
+app.use(bodyParser.json({ limit: '50mb' }));
 app.use(expressValidator());
 
 consign().include('routes').include('utils').into(app);
 
-app.listen(3000, 'localhost', () => {
+app.listen(4000, 'localhost', () => {
   console.log('Servidor rodando!');
 });
